6e0a765dcd729eff7d760c5d3a7d7fd66655c66d
Amine Bouhaik <154091532+aminebouhaik@users.noreply.github.com>
2024-09-28 17:14:31 +0200

    Update time.py

--- time.py
+++ time.py
@@ -1,9 +1,10 @@
 calculation_to_seconds = 24 * 60 * 60
 yes = "seconds"
 
-def day_per_seconds(number_of_days, message):
-    print(f"{number_of_days} days are {number_of_days * calculation_to_seconds} {yes}")
-    print(message)
+def day_per_seconds(number_of_days):
+  return f"{number_of_days} days are {number_of_days * calculation_to_seconds} {yes}"
 
-day_per_seconds(20, "awesome!")
-day_per_seconds(30, "looks good!")
+user_input=input("enter the number of days and i will convert it to seconds : ")
+user_input_number= int(user_input)
+calculation= day_per_seconds(user_input_number)
+print(calculation)
