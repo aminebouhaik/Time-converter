962ae2eff76a14d0c06e0626d2e6397e4a1cb14a
Amine Bouhaik <154091532+aminebouhaik@users.noreply.github.com>
2024-09-28 19:58:42 +0200

    Update time.py

--- time.py
+++ time.py
@@ -2,9 +2,12 @@ calculation_to_seconds = 24 * 60 * 60
 yes = "seconds"
 
 def day_per_seconds(number_of_days):
-  return f"{number_of_days} days are {number_of_days * calculation_to_seconds} {yes}"
+    if number_of_days > 0:
+        return f"{number_of_days} days are {number_of_days * calculation_to_seconds} {yes}"
+    else:
+        return "You've entered a negative value, so smahana no conversion for you !"
 
-user_input=input("enter the number of days and i will convert it to seconds : ")
-user_input_number= int(user_input)
-calculation= day_per_seconds(user_input_number)
+user_input = input("Enter the number of days and I will convert it to seconds: ")
+user_input_number = int(user_input)
+calculation = day_per_seconds(user_input_number)
 print(calculation)
